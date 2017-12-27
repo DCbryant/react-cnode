8c34002689aef4ebfd44f66854386ea66274bc51
dcbryant <dcbryant753@gmail.com>
2017-12-27 18:55:47 +0800

    add react config

---
+++ build/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry:{
+        app:path.join(__dirname,'../client/app.js'),
+    },
+    output:{
+        filename:'[name].[hash].js',
+        path:path.join(__dirname,'../dist'),
+        //publicPath加在我们引用前面的路径 
+        publicPath:'',
+    },
+    module:{
+        rules:[
+            {
+                test:/\.jsx$/,
+                loader:'babel-loader',
+            },
+            {
+                test:/\.js$/,
+                loader:'babel-loader',
+                exclude:[
+                    path.join(__dirname,'../node_modules')
+                ]
+            }
+        ]
+    },
+    plugins:[
+        new HtmlPlugin(),
+    ]
+}
