2bd9c3777200644dfd47287e7f786e910fc33944
dcbryant <dcbryant753@gmail.com>
2017-12-27 23:20:29 +0800

    完成热更新配置

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const HtmlPlugin = require('html-webpack-plugin')
 const isDev = process.env.NODE_ENV = 'development'
-
+const webpack = require('webpack')
 const config = {
     entry:{
         app:path.join(__dirname,'../client/app.js'),
@@ -10,7 +10,9 @@ const config = {
         filename:'[name].[hash].js',
         path:path.join(__dirname,'../dist'),
         //publicPath加在我们引用前面的路径 
-        publicPath:'/public',
+        // 这里不加/会导致HotModuleReplacementPlugin出现路径错误从而导致触发historyApiFallback
+        // 这个插件，从而让我们的热更新没有触发，导致页面每次更新
+        publicPath:'/public/',
     },
     module:{
         rules:[
@@ -37,11 +39,17 @@ const config = {
 // 同时还有一个坑，我们必须把dist删除掉，因为webpack默认先读取磁盘上的内容，然后才读取内存上的内容
 // 而webpack-dev-server需要读取内存上的内容，因为磁盘上的内容没有我们需要编译的内容
 if(isDev){
+    config.entry = {
+        app:[
+            'react-hot-loader/patch',
+            path.join(__dirname,'../client/app.js'),
+        ]
+    }
     config.devServer = {
         host:'0.0.0.0',
         port:'8888',
         contentBase:path.join(__dirname,'../dist'),
-        // hot:true,
+        hot:true,
         overlay:{
             errors:true
         },
@@ -51,6 +59,7 @@ if(isDev){
             index:'/public/index.html'
         }
     }
+    config.plugins.push(new webpack.HotModuleReplacementPlugin())
 }
 
 
--- client/app.js
+++ client/app.js
@@ -1,9 +1,32 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from './App.jsx'
+import {AppContainer} from 'react-hot-loader'
 
+const root = document.getElementById('root')
+const render = (Component,renderMethod) => {
+    ReactDOM[renderMethod](
+        <AppContainer>
+            <Component />
+        </AppContainer>,
+        root
+    )
+}
 
-ReactDOM.render(
-    <App />,
-    document.getElementById('root')
-)
+render(App,'render')
+
+// ReactDOM.render(
+//     <App />,
+//     document.getElementById('root')
+// )
+
+if(module.hot){
+    module.hot.accept('./App.jsx',() => {
+        const nextApp = require('./App.jsx').default
+        // ReactDOM.hydrate(
+        //     <App />,
+        //     document.getElementById('root')
+        // )
+        render(nextApp,'hydrate')
+    })
+}
